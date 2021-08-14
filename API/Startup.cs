4188ef66b80d76ddef6fc02e1b6f4ed16cc52dc0
Brandon <esolutions0510@gmail.com>
2021-08-14 15:53:28 -0500

    added generic repository and specifications

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
             services.AddSwaggerGen();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
